2e01652f28a84b8facd93bf67f81bf18eb54d030
Emmanuel Odhiambo <emmanuel3odhiambo@gmail.com>
2022-08-05 22:51:47 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Follow along with the examples here
+//function doNothing () {}
+
+//function sayHello() {
+ //   console.log("Hello!");
+//}
+//sayHello();
+
+function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+}
+
+function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+}
+
+function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+}
+
+
+sayHelloToIsabel();
+sayHelloToSofia();
+sayHelloToBrendan();
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+  
+  sayHelloTo("Isabel"); // "Hello, Isabel!"
+  sayHelloTo("Jane"); // "Hello, Jane!"
+  sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+  sayHelloTo(1); // "Hello, 1!"
+
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+
+  say("Goodbye", "Julio");
+
+  function add(x, y){
+    return x + y;
+ }
+
+ console.log(add(1,2))
